9fdb3b5f70e3611be5bd2d1105cd7ba41f2a1d0d
Wriprin <wriprin@gmail.com>
2020-12-05 08:45:33 +0800

    display songs' own albumPhoto

--- app/src/main/java/io/wriprin/android/ipod/AlbumDetails.java
+++ app/src/main/java/io/wriprin/android/ipod/AlbumDetails.java
@@ -1,14 +1,63 @@
 package io.wriprin.android.ipod;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.media.Image;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import static io.wriprin.android.ipod.MainActivity.musicFiles;
 
 public class AlbumDetails extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ImageView albumPhoto;
+    String albumName;
+    ArrayList<MusicFiles> albumSongs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album_details);
+        recyclerView = findViewById(R.id.recyclerView);
+        albumPhoto = findViewById(R.id.albumPhoto);
+        albumName = getIntent().getStringExtra("albumName");
+        int j = 0;
+        for (int i = 0 ; i < musicFiles.size() ; i ++)
+        {
+            if (albumName.equals(musicFiles.get(i).getAlbum()))
+            {
+                albumSongs.add(j, musicFiles.get(i));
+                j ++;
+            }
+        }
+        byte[] image = getAlbumArt(albumSongs.get(0).getPath());
+        if (image != null)
+        {
+            Glide.with(this)
+                    .load(image)
+                    .into(albumPhoto);
+        }
+        else
+        {
+            Glide.with(this)
+                    .load(R.drawable.bewedoc)
+                    .into(albumPhoto);
+        }
+    }
+
+    private byte[] getAlbumArt(String uri)
+    {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte[] art = retriever.getEmbeddedPicture();
+        retriever.release();
+        return art;
     }
 }
