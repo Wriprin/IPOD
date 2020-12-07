9e2ce69048bc6b3ece2cfaeea40cc7323924ad55
bixiTech <806925608@qq.com>
2020-12-07 13:52:37 +0800

    java连接包

---
+++ app/src/main/java/io/wriprin/android/ipod/DAO/User.java
@@ -0,0 +1,40 @@
+package com.example.mymusic.DAO;
+
+public class User {
+    private String password;
+    private String username;
+    private String name;
+
+    public User() {
+    }
+
+    public User(String password, String username, String name) {
+        this.password = password;
+        this.username = username;
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
