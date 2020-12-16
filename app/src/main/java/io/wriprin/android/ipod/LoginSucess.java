edd538bec05108d85ffbde06d2e3cbb828cc5c96
Wriprin <wriprin@gmail.com>
2020-12-16 20:48:47 +0800

    commit the FUNC of Login and Register.

---
+++ app/src/main/java/io/wriprin/android/ipod/LoginSucess.java
@@ -0,0 +1,16 @@
+package io.wriprin.android.ipod;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.wriprin.android.ipod.R;
+
+public class LoginSucess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sucess);
+    }
+}
